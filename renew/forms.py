97c1fffa7d62bed3854f4d71a9dedb2dc7d4ca1c
Chris Hartley <chris.hartley@anymouse.org>
2013-10-17 21:07:00 -0400

    Switched structure type to MultipleChoiceField based on types in db

--- renew/forms.py
+++ renew/forms.py
@@ -10,7 +10,7 @@ class SearchForm(ModelForm):
 	zipcode =  forms.ModelMultipleChoiceField(queryset=Zipcode.objects.all().order_by('name'), widget=forms.SelectMultiple)
 	cdc = forms.ModelMultipleChoiceField(queryset=CDC.objects.all().order_by('name'), widget=forms.SelectMultiple)
 	zone = forms.ModelMultipleChoiceField(queryset=Zoning.objects.filter(id__in=Property.objects.distinct('zone').values('zone').filter(propertyType__exact='lb')).order_by('name'), widget=forms.SelectMultiple)
-	structureType = forms.ModelMultipleChoiceField(queryset=Property.objects.distinct('structureType').value('structureType').order_by('structureType'), widget=forms.SelectMultiple)
+	structureType = forms.ModelMultipleChoiceField(queryset=Property.objects.distinct('structureType').order_by('structureType').filter(propertyType__exact='lb').values_list('structureType', flat=True), widget=forms.SelectMultiple)
 	minsize = forms.IntegerField(label="Minimum parcel size in square feet")
 	maxsize = forms.IntegerField(label="Maximum parcel size in square feet")
 	class Meta:
