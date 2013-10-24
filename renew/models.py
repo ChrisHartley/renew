dab82851609e922715c1813af3dd6b46f96801f3
Chris Hartley <chris.hartley@anymouse.org>
2013-10-24 01:11:38 -0400

    added help text, verbose names

--- renew/models.py
+++ renew/models.py
@@ -32,22 +32,18 @@ class Property(models.Model):
 	geometry = models.MultiPolygonField(srid=4326)
 	objects = models.GeoManager()
 
-	propertyType = models.CharField(choices=PROPERTY_TYPES, max_length=2)
+	propertyType = models.CharField(choices=PROPERTY_TYPES, max_length=2, verbose_name='property type')
 
-	parcel = models.CharField(max_length=7, unique=True)	
-	streetAddress = models.CharField(max_length=255)
-	nsp = models.NullBooleanField(default=False, null=True)
-	structureType = models.CharField(max_length=20, null=True, blank=True)
+	parcel = models.CharField(max_length=7, unique=True, help_text="The 7 digit local parcel number for a property, ex 1052714", verbose_name='parcel number')	
+	streetAddress = models.CharField(max_length=255, help_text="Supports partial matching, so you can enter either the full street address (eg 1425 E 11TH ST) to find one property or just the street name (eg 11th st) to find all the properties on that street.", verbose_name='street address')
+	nsp = models.NullBooleanField(default=False, null=True, help_text="If a property comes with requirements related to the Neighborhood Stabilization Program.", verbose_name='NSP')
+	structureType = models.CharField(max_length=20, null=True, blank=True, help_text="As classified by the Assessor", verbose_name='structure type')
 
-	cdc = models.ForeignKey(CDC, blank=True, null=True)
-	zone = models.ForeignKey(Zoning, blank=True, null=True)
-	zipcode = models.ForeignKey(Zipcode, blank=True, null=True)
+	cdc = models.ForeignKey(CDC, blank=True, null=True, help_text="The Community Development Corporation boundries the property falls within.", verbose_name='CDC')
+	zone = models.ForeignKey(Zoning, blank=True, null=True, help_text="The zoning of the property")
+	zipcode = models.ForeignKey(Zipcode, blank=True, null=True, help_text="The zipcode of the property")
 
-	@property
-	def _zipcode(self):
-		return Zipcode.objects.get(id=self.zipcode).name
-
-	area = models.FloatField()
+	area = models.FloatField(help_text="The parcel area in square feet")
 		
 	def __unicode__(self):
 		return '%s' % (self.parcel)
