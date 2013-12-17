9e4c26e50a8b50551e42768d088a25757e35f1f7
Chris Hartley <chris.hartley@anymouse.org>
2013-12-17 15:02:41 -0500

    added quiet_title_complete to renew_property

--- renew/models.py
+++ renew/models.py
@@ -37,6 +37,7 @@ class Property(models.Model):
 	parcel = models.CharField(max_length=7, unique=True, help_text="The 7 digit local parcel number for a property, ex 1052714", verbose_name='parcel number')	
 	streetAddress = models.CharField(max_length=255, help_text="Supports partial matching, so you can enter either the full street address (eg 1425 E 11TH ST) to find one property or just the street name (eg 11th st) to find all the properties on that street.", verbose_name='street address')
 	nsp = models.BooleanField(default=False, help_text="If a property comes with requirements related to the Neighborhood Stabilization Program.", verbose_name='NSP')
+	quiet_title_complete = models.BooleanField(default=False, help_text="If quiet title process has been completed.", verbose_name='Quiet Title Complete')
 	structureType = models.CharField(max_length=20, null=True, blank=True, help_text="As classified by the Assessor", verbose_name='structure type')
 
 	cdc = models.ForeignKey(CDC, blank=True, null=True, help_text="The Community Development Corporation boundries the property falls within.", verbose_name='CDC')
