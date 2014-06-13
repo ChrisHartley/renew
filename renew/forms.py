9ea7186d2b0dbc50b43bdf30b245d746dda69948
Chris Hartley <chris.hartley@anymouse.org>
2014-06-13 12:22:49 -0400

    add side lot related stuff

--- renew/forms.py
+++ renew/forms.py
@@ -14,9 +14,10 @@ class SearchForm(ModelForm):
 	minsize = forms.IntegerField(label="Minimum parcel size", help_text='Area is in square feet.')
 	maxsize = forms.IntegerField(label="Maximum parcel size", help_text='Area is in square feet.')
 	nsp = forms.ChoiceField(widget=forms.Select, label='NSP', help_text="If a property comes with requirements related to the Neighborhood Stabilization Program.", choices = (('1', 'Yes'), ('0', 'No'), ('', '') ), initial='')
+	sidelot_eligible = forms.ChoiceField(widget=forms.Select, label='Side lot', help_text="If a property is eligible for the side lot program.", choices = (('1', 'Yes'), ('0', 'No'), ('', '') ), initial='')
 	class Meta:
 		model = Property
-		fields = ['parcel', 'streetAddress', 'nsp', 'structureType', 'cdc', 'zone', 'zipcode']
+		fields = ['parcel', 'streetAddress', 'nsp', 'structureType', 'cdc', 'zone', 'zipcode', 'sidelot_eligible']
 
 
 
