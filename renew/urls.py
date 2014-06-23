4059d31e74e9115fe9cd657850a7845a431ef8ab
Chris Hartley <chris.hartley@anymouse.org>
2014-06-23 11:40:36 -0400

    add application_status view url

--- renew/urls.py
+++ renew/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib.gis import admin
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
+from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,7 +11,9 @@ urlpatterns = patterns('',
 	url(r'ajax/$', 'renew.views.showMapAjax'),
 	url(r'search/$', 'renew.views.search'),
 	url(r'search-form/$', 'renew.views.search_form'),
-
+	url(r'application_status/$', 'renew.views.showApplicationStatus'),
+   # Uncomment the next line to enable the admin:
+   # url(r'^admin/', include(admin.site.urls)),
 )
 
 
